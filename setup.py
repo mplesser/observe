652d5c4cfba0cb2559c8a229587e8b7d90d9474c
Michael Lesser <mlesser@email.arizona.edu>
2020-06-16 13:27:05 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="azcam-observe",
-    version="20.0",
+    name="observe",
+    version="20.1",
     description="azcam add-on package for running observing scripts",
     long_description=long_description,
     author="Michael Lesser",
