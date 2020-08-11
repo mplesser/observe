676488723b3c3ed933b72f29cd7529fc012eeae5
Michael Lesser <mlesser@email.arizona.edu>
2020-08-11 05:36:47 -0700

    cleanup

--- observe/make_gui.bat
+++ observe/make_gui.bat
@@ -1,3 +1,3 @@
 rem Compile pyqt UI
 
-C:\Python37\Scripts\pyside2-uic.exe -g python -o observe_gui_ui.py observe_gui.ui 
+C:\Python38\Scripts\pyside2-uic.exe -g python -o observe_gui_ui.py observe_gui.ui 
